a06fd775e1a18cf68a388ee5d1468310d436d241
Vettri <vettrivel18@gmail.com>
2020-03-01 17:55:07 +0530

    empty index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-const App = () =>{return (
-		<div>Hi there</div>
-	)}
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
