8e6d97ba3d0a184747da114789f0097a82d874e8
vkathirvelu <vettrivel18@gmail.com>
2020-05-22 20:18:23 +0530

    update with blog project

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { fetchPostsAndUser} from '../actions';
+import UserDetail from './UserDetail';
+
+
+class PostList extends React.Component{
+    componentDidMount(){
+        this.props.fetchPostsAndUser();
+    }
+
+    renderList(){
+      return this.props.posts.map(post =>{
+           return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content" >
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                    <div> <UserDetail userId={post.userId}/> </div>
+                </div>
+            );
+        });
+    }
+
+    render(){
+        return (
+            <div className="ui relaxed divided list">{this.renderList()}</div>
+        );
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return {posts : state.posts}
+}
+
+export default connect(mapStateToProps, {fetchPostsAndUser})(PostList);
---
+++ blog/src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers } from 'redux';
+import postReducers from './postsReducer';
+import userReducer from './userReducer';
+
+export default combineReducers({
+    posts : postReducers,
+    users : userReducer
+});
