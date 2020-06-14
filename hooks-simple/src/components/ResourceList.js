7c23c97b0f537b787ceafadc02fab900d7644c16
vkathirvelu <vettrivel18@gmail.com>
2020-06-14 11:22:28 +0530

    update project with hooks-simple

---
+++ hooks-simple/src/components/ResourceList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useResources from './useResources';
+
+const ResourceList = ({resource})=>{
+
+    const resources = useResources(resource);
+
+    return (<div>
+        <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
+    </div>);
+    
+}
+
+export default ResourceList;
---
+++ hooks-simple/src/components/useResources.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const useResources = (resource) =>{
+    const [resources, setResources] = useState([]);
+
+useEffect(() => {
+    (async () =>{
+        const response = await axios.get(`http://jsonplaceholder.typicode.com/${resource}`);
+        setResources(response.data);
+    })(resource)
+}, [resource]);
+
+return resources
+}
+
+export default useResources;
